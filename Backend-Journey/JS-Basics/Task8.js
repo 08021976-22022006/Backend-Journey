a408c01541751c1362b8b8d10ff3bd1c7113cfa4
08021976-22022006 <poojanas.dev@gmail.com>
2025-12-16 20:28:02 +0530

    Task 8

---
+++ Backend-Journey/JS-Basics/Task8.js
@@ -0,0 +1,41 @@
+// Use switch to print the season based on a month number.
+
+let m=7;
+switch(m){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("aug");
+        break;
+    case 9:
+        console.log("sept");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 11:
+        console.log("nov");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+}
